640381d7abdeb08839d5a67a489f1e4afe6e8a3f
Arun Kesavan <55449725+arunkottilukkal@users.noreply.github.com>
2022-09-19 19:15:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/home", method=["GET", "POST"])
+@app.route("/")
 def index():
-    return "Hello world"
+    return "Hello world!!!"
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=80)
+    app.run()
